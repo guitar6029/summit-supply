f5e95838def968fcb3ab0cc10c9cc41d3d6e1ab3
Josh <jsbusiness305@gmail.com>
2025-03-15 15:27:56 -0400

    added a new section, added bg img, updated responsive classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,21 @@
-
+import Link from "next/link";
 import Hero from "@/components/Hero/Hero";
-import Image from "next/image";
-import RidgeExplorer from "@/assets/img/ridgeExplorer_nobackground.png";
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Hero />
-      <section className="relative min-h-screen grid grid-cols-1 md:grid-cols-3 grid-row-2">
-          <div className="col-span-1 row-span-1">
-            <div className="text-[12rem] hiking-font text-shadow flex flex-col absolute left-10 z-[50] ">
-              {/* <span>Check Out</span>
-              <span>Our New Boots</span> */}
-              <span>Introducing the Ridge Explorer</span>
-              </div>
+      <section className="relative min-h-screen grid grid-cols-1  ridge-explorer-collection-bg">
+        <div className="col-span-1 flex flex-col justify-center">
+          <div className="text-7xl md:text-7xl lg:text-[12rem] hiking-font text-shadow flex flex-col p-10">
+            <span>Introducing the Ridge Explorer Collection</span>
+            <Link
+              href="/boots"
+              className="text-3xl md:text-5xl lg:text-6xl rounded-lg p-5 w-fit text-shadow-black bg-amber-600 hover:bg-amber-700 transition duration-300 ease-in"
+            >
+              Explore the Collections
+            </Link>
           </div>
-          <div className="col-span-3 h-[100vh] row-span-1">
-            <Image
-              src={RidgeExplorer}
-              alt="Ridge Explorer"
-              className="w-full h-full object-cover transition-transform duration-300 hover:scale-110"
-            />
-          </div>
-          
-      </section>
-
-      <section className="min-h-screen grid-cols-1 md:grid-cols-2 bg-[#0f3409]">
-                <div className="col-span-1">
-                  <h1 className="hiking-font text-[5rem] md:text-[10rem] text-wrap md:text-wrap text-shadow">Check out our blog</h1>
-                </div>
+        </div>
       </section>
     </div>
   );

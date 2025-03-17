c8c72a8b0b6c6c43a7779214dc0270b65cda2d0f
Josh <jsbusiness305@gmail.com>
2025-03-17 18:27:12 -0400

    added paypal package, wrapped app with the paypal provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navigation from "@/components/Navigation/Navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "@/components/Footer/Footer";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,15 +26,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const envKey = process.env.NEXT_PAYPAL_CLIENT_ID;
+  if (envKey === null || envKey === undefined) {
+    throw new Error("NEXT_PAYPAL_CLIENT_ID is not defined");
+  }
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}
       >
-        <Navigation />
-        <main className="min-h-screen mt-[5rem]">{children}</main>
-        <Footer />
-        <ToastContainer />
+        <PayPalScriptProvider options={{ clientId: envKey }}>
+          <Navigation />
+          <main className="min-h-screen mt-[5rem]">{children}</main>
+          <Footer />
+          <ToastContainer />
+        </PayPalScriptProvider>
       </body>
     </html>
   );

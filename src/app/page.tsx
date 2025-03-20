9f450da2b8c77b559fbf0a0d3f67f73fbf23eca5
Josh <jsbusiness305@gmail.com>
2025-03-20 17:58:54 -0400

    updated the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <section className="relative min-h-screen grid grid-cols-1 ridge-explorer-collection-bg mt-10">
         <div className="col-span-1 flex flex-col justify-center">
           <div className="text-7xl md:text-7xl lg:text-[10rem] hiking-font text-shadow flex flex-col p-10">
-            <span>Introducing the Ridge Explorer Collection</span>
+            <span>Introducing the Ridge Explorer</span>
             <Link
-              href="/shoes/boots"
+              href="/boots"
               className="text-3xl md:text-5xl lg:text-6xl rounded-lg p-5 w-fit text-shadow-black bg-amber-600 hover:bg-amber-700 transition duration-300 ease-in"
             >
               Explore the Collections

f9f5406f149c43b3e5463704c109510def31c68e
Josh <jsbusiness305@gmail.com>
2025-03-19 21:55:54 -0400

    commend out Account component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,15 +6,15 @@ import SupplySummitLogo from "../Logo/SupplySummitLogo";
 export default async function Navigation() {
   return (
     <div className="flex flex-row items-center justify-around h-[100px] fixed top-0 w-full bg-[var(--desaturated-blue)] z-[20]">
-      <div className="hidden md:flex flex-row items-center gap-10">
-        <div className="hidden md:flex flex-row items-center">
+      <div className="flex flex-row items-center gap-10">
+        <div className="flex flex-row items-center">
           <SupplySummitLogo className="w-[100px]" />
         </div>
 
         <ListLinks links={myNavigationLinks} />
       </div>
 
-      <Account />
+      {/* <Account /> */}
     </div>
   );
 }

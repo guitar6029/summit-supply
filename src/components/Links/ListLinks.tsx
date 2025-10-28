854352e929ceb64dd712a629e2c7e990594a1170
Josh <jsbusiness305@gmail.com>
2025-10-28 13:30:50 -0400

    fixed hydration issue

--- src/components/Links/ListLinks.tsx
+++ src/components/Links/ListLinks.tsx
@@ -1,16 +1,20 @@
 "use client";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import CustomLink from "./CustomLink";
 import useCart from "@/store/useCart";
 import { Backpack } from "lucide-react";
+
 export default function ListLinks({
   links,
 }: {
   links: { name: string; href: string }[];
 }) {
   const { shoppingCartIsEmpty } = useCart();
-
   const pathname = usePathname();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
 
   function getLinkClass() {
     if (
@@ -27,16 +31,17 @@ export default function ListLinks({
   return (
     <div className="flex flex-row gap-4">
       {links.map((link) => {
+        const showCartIcon =
+          mounted && link.name === "Cart" && !shoppingCartIsEmpty();
+
         return (
           <CustomLink
             href={link.href}
             key={link.name}
-            className={`${getLinkClass()}"} flex flex-row items-baseline gap-2 transition duration-300 ease-in hiking-font text-2xl`}
+            className={`${getLinkClass()} flex flex-row items-baseline gap-2 transition duration-300 ease-in hiking-font text-2xl`}
           >
             {link.name}
-            {link.name === "Cart" && !shoppingCartIsEmpty() && (
-              <Backpack size={20} />
-            )}
+            {showCartIcon && <Backpack size={20} />}
           </CustomLink>
         );
       })}
